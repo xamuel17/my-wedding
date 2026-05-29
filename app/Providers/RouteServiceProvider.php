b608deadf7962ecc17fa7808734d9cf0fccc89eb
Samuel Unachukwu <samuel.unachukwu@vfdtech.ng>
2026-05-29 01:45:38 +0100

    u

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,6 +14,10 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
         });
     }
 }
